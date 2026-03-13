edd8ee721fa3be9363027518fc5e17342349da6b
DEVLOPER Anmol Shisodia <anmolthakur1551@gmail.com>
2026-03-13 11:04:03 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,11 @@
+//JavaScript Code 
+   //Dark Mode feature
+    function changeTheme(mode){
+        let a=document.body.style.backgroundColor
+        if (a===""||a==="white") {
+            document.body.style.backgroundColor="black"
+        } else {
+            document.body.style.backgroundColor="white"
+        }
+    }
+   // alert("This is Cloning E-commerce Website")
